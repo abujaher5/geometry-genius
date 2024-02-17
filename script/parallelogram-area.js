85ac92169e27900f625cfffa1691b89cabdc30b1
abujaher5 <abujaher5120@gmail.com>
2024-02-17 17:24:53 +0600

    added parallelogram card

--- script/parallelogram-area.js
+++ script/parallelogram-area.js
@@ -1,22 +1,57 @@
+// function calculateParallelogramArea() {
+//   //get parallelogram base value
+//   const parallelogramBaseInput = document.getElementById("parallelogram-base");
+//   const parallelogramBaseText = parallelogramBaseInput.value;
+//   const base = parseFloat(parallelogramBaseText);
+//   console.log(base);
+
+//   //get parallelogram height value
+//   const parallelogramHeightInput = document.getElementById(
+//     "parallelogram-height"
+//   );
+//   const parallelogramHeightText = parallelogramHeightInput.value;
+//   const height = parseFloat(parallelogramHeightText);
+//   console.log(height);
+//   //calculate the area of parallelogram
+//   const area = base * height;
+//   console.log("area of the parallelogram is", area);
+
+//   //display parallelogram area
+//   const parallelogramAreaSpan = document.getElementById("parallelogram-area");
+//   parallelogramAreaSpan.innerText = area;
+//
+
+//   const base = getParallelogramBase();
+
+//   console.log(base);
+// }
+
+// function getParallelogramBase() {
+//   const baseInput = document.getElementById("parallelogram-base");
+//   const baseText = baseInput.value;
+//   const base = parseFloat(baseText);
+//   return base;
+// }
+
 function calculateParallelogramArea() {
-  //get parallelogram base value
-  const parallelogramBaseInput = document.getElementById("parallelogram-base");
-  const parallelogramBaseText = parallelogramBaseInput.value;
-  const base = parseFloat(parallelogramBaseText);
-  console.log(base);
+  const base = getInputValueById("parallelogram-base");
+  console.log("base value", base);
 
-  //get parallelogram height value
-  const parallelogramHeightInput = document.getElementById(
-    "parallelogram-height"
-  );
-  const parallelogramHeightText = parallelogramHeightInput.value;
-  const height = parseFloat(parallelogramHeightText);
-  console.log(height);
-  //calculate the area of parallelogram
+  const height = getInputValueById("parallelogram-height");
+  console.log("height value", height);
   const area = base * height;
-  console.log("area of the parallelogram is", area);
+  console.log("area of parallelogram is", area);
+  setInnerTextById("parallelogram-area", area);
+}
 
-  //display parallelogram area
-  const parallelogramAreaSpan = document.getElementById("parallelogram-area");
-  parallelogramAreaSpan.innerText = area;
+function getInputValueById(inputFieldId) {
+  const inputField = document.getElementById(inputFieldId);
+  const inputValueText = inputField.value;
+  const inputValue = parseFloat(inputValueText);
+  //   console.log(inputValue);
+  return inputValue;
+}
+function setInnerTextById(elementId, area) {
+  const element = document.getElementById(elementId);
+  element.innerText = area;
 }
